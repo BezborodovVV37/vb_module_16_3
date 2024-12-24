c67caef566f6f76022815f4f834bfa95e3e4a70d
BezborodovVV37 <blood371993@gmail.com>
2024-12-24 10:47:10 +0300

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, Path, HTTPException
-from typing import Annotated
+from fastapi import FastAPI, Path, HTTPException, Query
+from typing import Dict
 
 app = FastAPI()
 
 # Для запуска сервера, в терминале используйте команду: uvicorn module_16_3:app --reload
 
-users = {'1': 'Имя: Example, возраст: 18'}
+users: Dict[str, str] = {'1': 'Имя: Example, возраст: 18'}
 
 
 @app.get('/users')
@@ -15,8 +15,8 @@ async def get_users() -> dict:
 
 @app.post('/user/{username}/{age}')
 async def post_users(
-        username: Annotated[str, Path(description="Имя пользователя")],
-        age: Annotated[int, Path(description="Возраст пользователя")]) -> str:
+        username: str = Path(..., min_length=5, max_length=20, description="Enter username", example="UrbanUser "),
+        age: int = Path(..., ge=18, le=120, description="Enter age", example=24)) -> str:
     current_index = str(int(max(users, key=int)) + 1)
     users[current_index] = f"Имя: {username}, возраст: {age}"
     return f'User  {current_index} is registered'
@@ -24,22 +24,21 @@ async def post_users(
 
 @app.put('/user/{user_id}/{username}/{age}')
 async def update_user(
-        user_id: Annotated[str, Path(description="ID пользователя")],
-        username: Annotated[str, Path(description="Имя пользователя")],
-        age: Annotated[int, Path(description="Возраст пользователя")]) -> str:
-    if user_id not in users:
+        user_id: int = Path(..., ge=1, le=100, description="Enter User ID", example=1),
+        username: str = Path(..., min_length=5, max_length=20, description="Enter username", example="UrbanUser "),
+        age: int = Path(..., ge=18, le=120, description="Enter age", example=24)) -> str:
+    if str(user_id) not in users:
         raise HTTPException(status_code=404, detail=f"User  {user_id} not found")
 
-    users[user_id] = f"Имя: {username}, возраст: {age}"
+    users[str(user_id)] = f"Имя: {username}, возраст: {age}"
     return f"User  {user_id} has been updated"
 
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: Annotated[str, Path(description="ID пользователя")]) -> str:
-    if user_id not in users:
+async def delete_user(
+        user_id: int = Path(..., ge=1, le=100, description="Enter User ID", example=1)) -> str:
+    if str(user_id) not in users:
         raise HTTPException(status_code=404, detail=f"User  {user_id} not found")
 
-    users.pop(user_id)
+    users.pop(str(user_id))
     return f"User  {user_id} has been deleted"
-
-
